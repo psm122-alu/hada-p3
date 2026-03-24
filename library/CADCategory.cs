d17423101aea9bd25dd4606972b3ef495866698a
prg88-ua <prg88@alu.ua.es>
2026-03-24 19:07:05 +0100

    46081260R Clase CADCategory acabada

--- library/CADCategory.cs
+++ library/CADCategory.cs
@@ -3,10 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace library
 {
     internal class CADCategory
     {
+        private string conexion;
+
+        public CADCategory()
+        {
+            conexion = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        public bool read(ENCategory en)
+        {
+            bool leer = false;
+            SqlConnection conn = null;
+
+            String comando = "Select * from Categories where id = " + en.id;
+
+            try
+            {
+                conn = new SqlConnection(conexion);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader datos_leer = cmd.ExecuteReader();
+
+                if (datos_leer.Read())
+                {
+                    en.name = datos_leer["name"].ToString();
+                    leer = true;
+                }
+
+                datos_leer.Close();
+            }
+            catch (SqlException sqlex)
+            {
+                throw new Exception("FALLO AL LEER: " + en.id, sqlex);
+            }
+
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+            return leer;
+        }
+
+        public List<ENCategory> readAll()
+        {
+            List<ENCategory> lista = new List<ENCategory>();
+            SqlConnection conn = null;
+
+            string comando = "Select * from Categories";
+
+            try
+            {
+                conn = new SqlConnection(conexion);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader datos_leer = cmd.ExecuteReader();
+
+                while (datos_leer.Read())
+                {
+                    ENCategory categoria = new ENCategory();
+                    categoria.id = int.Parse(datos_leer["id"].ToString());
+                    categoria.name = datos_leer["name"].ToString();
+                    lista.Add(categoria);
+                }
+                datos_leer.Close();
+            }
+            catch (SqlException sqlex) { throw new Exception("FALLO EN CONSULTA", sqlex); }
+            catch (Exception ex) { throw ex; }
+
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+            return lista;
+
+        }
     }
 }
